9097f13d5ccb198e38e0779453b7f72054267d83
Raimu Kinoshita <raijirou12@gmail.com>
2024-10-10 21:20:03 +0900

    fibfen.pyを制作した

---
+++ num2/fibgen.py
@@ -0,0 +1,22 @@
+class FibGenerator:
+    _num: int
+    def __init__(self, num: int):
+        self._num = num
+
+    def __iter__(self):
+        i = 0
+        j = 1
+        times = self._num
+        while times > 0:
+            yield j
+            tmp = j
+            j = i + j
+            i = tmp
+            times -= 1
+
+
+
+
+if __name__=="__main__":
+    for v in FibGenerator(10):
+        print(v)
